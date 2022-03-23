0e92bcb3e37ba447c260871e9a956c83350305f1
Hayden Hudson <hhudson@uchicago.edu>
2022-03-23 11:05:17 -0400

    CORRECTIONS

--- views/v_ast_db_plsql_2_discouraged_code.sql
+++ views/v_ast_db_plsql_2_discouraged_code.sql
@@ -1,5 +1,5 @@
 /* this view is referenced in v_ast_db_plsql_all */
-create or replace force view v_ast_db_plsql_2_deprecated_code as
+create or replace force view v_ast_db_plsql_2_discouraged_code as
 select
 case when lower(us.text) like '%nvl%'
      then 'Replace with coalesce'
@@ -12,7 +12,8 @@ us.type object_type,
 us.name object_name, 
 us.text code, 
 us.line,
-'Checking for Deprecated Code' check_type
+'Checking for Discouraged Code' check_type
 from user_source us
+where us.name not like 'BIN$%'
 order by us.type, us.name, us.text, us.line
 ;
